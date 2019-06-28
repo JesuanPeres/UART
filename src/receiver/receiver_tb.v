bc290a34e30c0bd92ea66020249a342360f893f0
MrPontas <guipontarolo@gmail.com>
2019-06-27 22:48:38 -0300

    Testbench receiver atualizado.

--- src/receiver/receiver_tb.v
+++ src/receiver/receiver_tb.v
@@ -2,9 +2,11 @@
 
 
 module receiver_tb();
+	reg RXD, clk, reset;
+	wire rx_busy;
 	receiver r1(
-		.RXD(1'b0),
-		.clk(1'b0),
+		.RXD(RXD),
+		.clk(clk),
 		.reset(1'b0)
 	);
 
@@ -12,5 +14,19 @@ module receiver_tb();
 	initial begin
 		$dumpfile("receiver_tb.vcd");
 		$dumpvars(0,receiver_tb);
+		clk = 0;
+		//#20 send = 1;
+		#20 RXD = 1;
+		#20 RXD = 0;
+		#20	RXD = 1;
+		#20 RXD = 0;
+		#20 RXD = 1;
+		#20 RXD = 0;
+		#20 RXD = 1;
+		#20 RXD = 1;
+		#200	
+		$finish;
+		
 	end
+	always #10 clk =!clk;
 endmodule
